5294f65bfb40257c5a098453160e6f43e9281152
ilikeavocadoes <ilikeavocadoes@hush.com>
2020-12-06 15:11:41 +0200

    Day 1.1

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -3,4 +3,21 @@ module Main where
 import Common
 
 main :: IO ()
-main = test
+main = do
+  numbers <- input
+  let (x, y) =  correctPair $ numberPairs numbers
+  print $ x * y
+
+input :: IO [Integer]
+input = do
+  contents <- readFile "input/day1.1"
+  return $ map read $ lines contents
+
+numberPairs :: [Integer] -> [(Integer, Integer)]
+numberPairs numbers = [ (x, y) | x <- numbers, y <- numbers ]
+
+correctPair :: [(Integer, Integer)] -> (Integer, Integer)
+correctPair numbers = head $ filter sumsTo2020 numbers
+
+sumsTo2020 :: (Integer, Integer) -> Bool
+sumsTo2020 (x, y) = x + y == 2020
